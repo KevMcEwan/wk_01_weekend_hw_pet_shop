e6922a771b13b13274d2f53d14f3410cdcd28483
KevMcEwan <kev_mcewan@hotmail.com>
2018-10-19 13:53:31 +0100

    First two tests completed

---
+++ start_point/pet_shop.rb
@@ -0,0 +1,10 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+
+def total_cash (amount)
+  return  amount[:admin][:total_cash]
+end
+
+def add_or_remove_cash(total_cash, amount_to_add)
